50679feb7608f488400b534ff59c35393d2acad1
Saragog <andrzejdackiewicz@gmail.com>
2017-10-12 14:57:07 +0200

    Cant find a mistake :( but apparently something is still wrong ?

--- src/Freedom.java
+++ src/Freedom.java
@@ -49,11 +49,12 @@ Output:
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 class Freedom {
@@ -61,41 +62,130 @@ class Freedom {
 	private static BufferedReader bufferedReader;
 	private static StringTokenizer stringTokenizer;
 	private LinkedList<Integer> pilotsSkills;
-	private int interceptorNumber;
+	private int pilotsNumber;
+	private LinkedList<State> foundStates;
+	private Map<List<Integer>, Boolean> assignmentAlreadyAnalyzed; // There already was such assignment of pilots so does not need repetition
+	private int bestPilotSkill;
+	
+	private class State
+	{
+		public int skillSumTeam1;
+		public int skillSumTeam2;
+		public int pilotsAssigned;
+		public List<Integer> pilotsAssignments;
+		
+		public State(int skillSumTeam1, int skillSumTeam2, List<Integer> pilotsAssignments, int pilotsAssigned)
+		{
+			this.skillSumTeam1 = skillSumTeam1;
+			this.skillSumTeam2 = skillSumTeam2;
+			this.pilotsAssignments = pilotsAssignments;
+			this.pilotsAssigned = pilotsAssigned;
+		}
+	}
 	
 	private Freedom()
 	{
 		pilotsSkills = new LinkedList<Integer>();
-    //  this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    // 	this.stringTokenizer = new StringTokenizer(this.bufferedReader.readLine());
+		foundStates = new LinkedList<State>();
+		assignmentAlreadyAnalyzed = new HashMap<List<Integer>, Boolean>();
     }
 	
 	private void readSkills()
 	{
+		assignmentAlreadyAnalyzed.clear();
 		pilotsSkills.clear(); // Reading pilot skill levels
-		this.interceptorNumber = Integer.parseInt(stringTokenizer.nextToken());
-		while (interceptorNumber-- > 0) // TODO later on create our own insertion sort for fun and training :D
-			pilotsSkills.add(Integer.parseInt(stringTokenizer.nextToken()));
+		this.pilotsNumber = Integer.parseInt(stringTokenizer.nextToken());
+		boolean bestPilotSkillWasSet = false;
+		int readSkill;
+		for (int pilotsSkillsToBeRead = pilotsNumber; pilotsSkillsToBeRead > 0; pilotsSkillsToBeRead--) // TODO later on create our own insertion sort for fun and training :D
+		{
+			readSkill = Integer.parseInt(stringTokenizer.nextToken());
+			pilotsSkills.add(readSkill);
+			if (!bestPilotSkillWasSet)
+			{
+				bestPilotSkill = readSkill;
+				bestPilotSkillWasSet = true;
+			}
+			else if (bestPilotSkill < readSkill) bestPilotSkill = readSkill;
+		}
+	}
+	
+	private List<Integer> prepareNextPilotsAssignments(List<Integer> pilotsAssignments, int assignedPilotIndex, int pilotsSquadron)
+	{
+		List<Integer> nextPilotsAssignments = new ArrayList<Integer>(pilotsNumber);
+		for (int actualPilotIndex = 0; actualPilotIndex < pilotsNumber; actualPilotIndex++)
+		{
+			if (actualPilotIndex == assignedPilotIndex) nextPilotsAssignments.add(actualPilotIndex, pilotsSquadron);
+			else nextPilotsAssignments.add(actualPilotIndex, pilotsAssignments.get(actualPilotIndex).intValue());
+		}
+		return nextPilotsAssignments;
+	}
+	
+	private void assignNextPilot(List<Integer> pilotsAssignments, int pilotSkill, int pilotIndex, int skillSumTeam1, int skillSumTeam2, int pilotsAssigned)
+	{
+		List<Integer> assignmentKey = prepareNextPilotsAssignments(pilotsAssignments, pilotIndex, 1);
+		if (!assignmentAlreadyAnalyzed.containsKey(assignmentKey))
+		{
+			foundStates.add(new State(skillSumTeam1 + pilotSkill, skillSumTeam2, assignmentKey, pilotsAssigned));
+			assignmentAlreadyAnalyzed.put(assignmentKey, true);
+		}
+		assignmentKey = prepareNextPilotsAssignments(pilotsAssignments, pilotIndex, 2);
+		if (!assignmentAlreadyAnalyzed.containsKey(assignmentKey))
+		{
+			foundStates.add(new State(skillSumTeam1, skillSumTeam2 + pilotSkill, assignmentKey, pilotsAssigned));
+			assignmentAlreadyAnalyzed.put(assignmentKey, true);
+		}
+		
+		return;
+	}
+	
+	
+	
+	private void findNewStates(State analyzedState)
+	{
+		int skillSumTeam1, skillSumTeam2, pilotsAssigned, pilotSkill;
+		List<Integer> pilotsAssignments = analyzedState.pilotsAssignments;
+		skillSumTeam1 = analyzedState.skillSumTeam1;
+		skillSumTeam2 = analyzedState.skillSumTeam2;
+		pilotsAssigned = analyzedState.pilotsAssigned;
+		Iterator<Integer> iterator = pilotsSkills.iterator();
+		for (int pilotIndex = 0; pilotIndex < pilotsNumber; pilotIndex++)
+		{
+			pilotSkill = iterator.next();
+			if (pilotsAssignments.get(pilotIndex).intValue() == 0)
+				assignNextPilot(pilotsAssignments, pilotSkill, pilotIndex, skillSumTeam1, skillSumTeam2, pilotsAssigned+1);
+		}
 	}
 	
 	private int divideInterceptorsIntoTwoSquadrons()
 	{
-		int combinedTeamSkill1, combinedTeamSkill2;
-		Iterator<Integer> iterator = pilotsSkills.iterator();
-		for (combinedTeamSkill1 = iterator.next().intValue(), combinedTeamSkill2 = 0;
-			 iterator.hasNext();)
-			if (combinedTeamSkill1 <= combinedTeamSkill2) combinedTeamSkill1 += iterator.next().intValue(); 
-			else combinedTeamSkill2 += iterator.next().intValue();
-		System.out.println("Suma1: " + combinedTeamSkill1 + " Suma2: " + combinedTeamSkill2);
-		if (combinedTeamSkill1 >= combinedTeamSkill2) return combinedTeamSkill1 - combinedTeamSkill2;
-		else return combinedTeamSkill2 - combinedTeamSkill1;
+		int minimum = bestPilotSkill, difference;
+		int difference1minus2, difference2minus1;
+		State analyzedState;
+		foundStates.clear();
+		List<Integer> pilotsAssignments = new ArrayList<Integer>(pilotsNumber);
+		for (int pilot = 0; pilot < pilotsNumber; pilot++)
+			pilotsAssignments.add(pilot, 0);
+		foundStates.add(new State(0, 0, pilotsAssignments, 0));
+		assignmentAlreadyAnalyzed.put(pilotsAssignments, true);
+		while (!foundStates.isEmpty())
+		{
+			analyzedState = foundStates.removeFirst();
+			if (analyzedState.pilotsAssigned == pilotsNumber)
+			{
+				difference1minus2 = analyzedState.skillSumTeam1 - analyzedState.skillSumTeam2;
+				difference2minus1 = analyzedState.skillSumTeam2 - analyzedState.skillSumTeam1;
+				difference = (difference1minus2 >= difference2minus1)? difference1minus2 : difference2minus1;
+				if (difference < minimum) minimum = difference;
+			}
+			else findNewStates(analyzedState);
+		}
+		return minimum;
 	}
 	
 	private void performOneTest()
 	{
 		readSkills();
-		Collections.sort(pilotsSkills);
-		Collections.reverse(pilotsSkills);
 		System.out.println(divideInterceptorsIntoTwoSquadrons());
 	}
 	
